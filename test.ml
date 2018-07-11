6e59d90e27bacf95cc5e19a7072b7703ece42f01
Pierre Letouzey <pierre.letouzey@inria.fr>
2018-07-11 20:21:15 +0200

    still more experiments

--- test.ml
+++ test.ml
@@ -2,10 +2,33 @@
 let float = float_of_int
 let int = int_of_float
 
+(** Utilities *)
+
+let histo a =
+  let t = Array.copy a in
+  let () = Array.sort compare t in
+  let rec cumul a k i =
+    if i >= Array.length t then [(a,k)]
+    else if t.(i) = a then cumul a (k+1) (i+1)
+    else (a,k)::cumul t.(i) 1 (i+1)
+  in cumul t.(0) 1 1
+
+let extrem t =
+  let mi = ref t.(0) and mx = ref t.(0) in
+  for i = 1 to Array.length t -1 do
+    mi := min t.(i) !mi;
+    mx := max t.(i) !mx
+  done;
+  (!mi,!mx)
+
+(* *)
 
 let rec d n = if n = 0 then 0 else n - d(n-1);;
+
+(* http://oeis.org/A005206 *)
 let rec g n = if n = 0 then 0 else n - g (g (n-1));;
 
+(* http://oeis.org/A005374 *)
 let rec h n = if n = 0 then 0 else n - h (h (h (n-1)));;
 
 let rec iter f n = if n=1 then f else fun x -> iter f (n-1) (f x);;
@@ -13,11 +36,14 @@ let rec iter f n = if n=1 then f else fun x -> iter f (n-1) (f x);;
 let rec f k n = if k=0 then n else if n = 0 then 0 else
             n - iter (f k) k (n-1)
 
+(* nb: f4 is http://oeis.org/A005375 *)
+
 let _ = Array.init 20 @@ fun n -> n, d n
 let _ = Array.init 20 @@ fun n -> n,f 6 n
 let _ = Array.init 20 @@ f 2
 let _ = Array.init 20 h
 let _ = Array.init 20 @@ f 3
+let _ = Array.init 20 @@ f 4
 
 let _ = Array.init 100 @@ fun n -> g n - d n
 
@@ -28,13 +54,16 @@ let _ = Array.init 100 @@ fun n -> g n - d n
 let phi = (1.+.sqrt(5.))/.2. (* root of X^2-X-1 *)
 let tau = phi-.1.  (* also 1/phi, root of X^2+X-1 *)
 
+let _ = g 22
+
+
 (* k = 3 *)
 let limh = 0.6823278038280194 (* root of X^3+X-1, cf maxima *)
 let expo = 1.465571231876768 (* 1/limh, also root of X^3-X^2-1 *)
 
 (* k = 4 *)
-
 let expo4 = 1.380277569097618
+
 
 (** Little equation solver (via Newton's method.
    newton k find an approximation of the root of X^k+X-1
@@ -77,6 +106,7 @@ let tabulate k n =
   a
 
 let a = tabulate 1000 1000
+let aa = tabulate 6 100000
 
 let check k n a =
   for i = 1 to k-2 do
@@ -112,7 +142,7 @@ donc floor ((n+1)/phi) is either floor(n/phi) or
 
 let t2 = Array.init 1000 @@ fun i -> int ( float (i+1) /. phi)
 
-let t2' = Array.init 1000 @@ fun i -> float i /. phi)
+let t2' = Array.init 1000 @@ fun i -> float i /. phi
 
 let _ = t2 = a.(2)
 
@@ -168,10 +198,15 @@ let hter n = int (ceil (limh *. float n))
 
 let _ = Array.init 20 @@ fun n -> (hbis n, h n, hter n)
 
-let _ = Array.init 1000 @@ fun n ->
+let out = Array.init 1000 @@ fun n ->
         if a.(3).(n) = hbis n then F
         else if a.(3).(n) = hter n then C
         else failwith "AWAY"
+
+let out = Array.init 100000 @@ fun n ->
+                             float aa.(3).(n) -. limh *. float n
+
+let _ = extrem out
 
 let _ = Array.init 1000 @@ fun n ->
         int (limh *. (float (n+1))) - a.(3).(n)
@@ -184,18 +219,19 @@ let out = Array.init 1000 @@ fun n ->
         a.(4).(n) - int(float n /. expo4)
   (* histo : (-1, 4); (0, 504); (1, 482); (2, 10) *)
 
+let out = Array.init 100000 @@ fun n ->
+        aa.(4).(n) - int(float n /. expo4)
+  (* histo : (-1, 746); (0, 49930); (1, 47971); (2, 1353) *)
+
+let _ = histo out
+
 let out = Array.init 1000 @@ fun n ->
         a.(5).(n) - int(float n *. lims.(5))
   (* histo : (-1, 55); (0, 475); (1, 411); (2, 59) *)
 
-let histo a =
-  let t = Array.copy a in
-  let () = Array.sort compare t in
-  let rec cumul a k i =
-    if i >= Array.length t then [(a,k)]
-    else if t.(i) = a then cumul a (k+1) (i+1)
-    else (a,k)::cumul t.(i) 1 (i+1)
-  in cumul t.(0) 1 1
+let out = Array.init 100000 @@ fun n ->
+        aa.(5).(n) - int(float n *. lims.(5))
+  (* histo : (-2, 712); (-1, 15182); (0, 42821); (1, 33705); (2, 7345); (3, 235) *)
 
 let _ = histo out
 
@@ -206,53 +242,71 @@ let _ = histo out
 (** Zeckendorf decomposition of rank 3 *)
 
 (** The sequence.
-    Similar to Fibonacci, but skipping a term in the addition *)
-let rec a n = if n<4 then n else a (n-1) + a (n-3)
+    Similar to Fibonacci, but skipping a term in the addition
+    See http://oeis.org/A000930 (Narayana)
+
+ *)
+let rec s4 n = if n<4 then n else s4 (n-1) + s4 (n-3)
 
 (** Ratio converges to expo *)
-let _ = Array.init 20 (fun n -> float (a (n+2)) /. float (a (n+1)))
+let _ = Array.init 20 (fun n -> float (s4 (n+2)) /. float (s4 (n+1)))
 
 (** Memoized version of the sequence *)
-let atab = Array.make 100 0
+let s4tab = Array.make 100 0
 
-let rec aopt n =
-  if n >= 100 then a n
+let rec s4opt n =
+  if n >= 100 then s4 n
   else
-    let x = atab.(n) in
+    let x = s4tab.(n) in
     if x<>0 then x
     else
-      if n <4 then (atab.(n) <- n; n)
+      if n <4 then (s4tab.(n) <- n; n)
       else
-        let x = aopt (n-1) + aopt (n-3) in
-        (atab.(n) <- x; x)
+        let x = s4opt (n-1) + s4opt (n-3) in
+        (s4tab.(n) <- x; x)
 
-let _ = aopt 70
-let _ = atab
+let _ = s4opt 70
+let _ = s4tab
+let _ = s4opt 99
+let _ = s4tab
 
-let _ = Array.mapi (fun i x -> float x /.(expo ** float i)) atab
+let _ = Array.init 100 @@ fun i -> float s4tab.(i) /.(expo ** float i)
 
-(** a(n) ~ 0.89618 * expo^n *)
+(** s4(n) ~ 0.89618507192613 * expo^n *)
 
 (** The decomposition *)
 
 let rec decomp n =
   let rec loop k =
-    if aopt k > n then k-1
+    if s4opt k > n then k-1
     else loop (k+1)
   in
   if n = 0 then []
   else
     let x = loop 1 in
-    x::(decomp (n-aopt x))
+    x::(decomp (n-s4opt x))
 
-let _ = Array.init 100 (fun n -> n, decomp n)
+let _ = Array.init 100 @@ fun n -> n, decomp n
 
-let _ = atab.(0) <- 0
+let a20942 =
+  let l = Array.to_list (Array.init 100 @@ fun n -> n, decomp n) in
+  List.filter (fun (x,l) -> List.mem 1 l) l
+
+(* See http://oeis.org/A020942 : numbers containing 1 in
+   their rank-3 decomposition.
+   = places where h will stagnate: h 1 = h 2, h 5 = h 6, h 7 = h 8, etc
+*)
+
+let _ = List.map (fun (n,_) -> a.(3).(n+1) - a.(3).(n)) a20942
+
+
+
+let _ = s4tab.(0) <- 0
 
 let prevdecomp l = List.map (function 1 -> 1 | n -> n-1) l
-let sumdecomp l = List.fold_left (fun acc n -> acc + aopt n) 0 l
+let sumdecomp l = List.fold_left (fun acc n -> acc + s4opt n) 0 l
 
-let _ = List.map (fun n -> h n, sumdecomp (prevdecomp (decomp n))) test
+let _ = Array.init 20 @@ fun n -> h n, sumdecomp (prevdecomp (decomp n))
 
 
 
